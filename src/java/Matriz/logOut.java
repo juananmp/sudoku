9129e082d288d1c81966c6510935b57e0a586858
janto <ja.merino@usp.ceu.es>
2017-12-07 00:14:32 +0100

    nueva servlet logout la cual cerrara la sesion del sudoku

---
+++ src/java/Matriz/logOut.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Matriz;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author janto
+ */
+public class logOut extends HttpServlet {
+
+   
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        HttpSession cliente = request.getSession();
+        cliente.invalidate();
+        out.println("Has hecho logOut");
+        out.close();
+        
+    }
+
+    
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
